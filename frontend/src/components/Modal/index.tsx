d013b68518d22feb6d14b7c51a40150574ff9a6c
sounishnath003 <flock.sinasini@gmail.com>
2021-03-14 15:00:15 +0530

    MODAL: create new label via modal

--- frontend/src/components/Modal/index.tsx
+++ frontend/src/components/Modal/index.tsx
@@ -1,14 +1,86 @@
 import React from "react";
-import './styles.css';
+import { InfoIcon } from "../../assets/icons";
+import "./styles.css";
 
-function Modal() {
+interface IProps {
+  title: string;
+  closeBind: React.Dispatch<React.SetStateAction<boolean>>;
+  saveBind: (data: string) => void;
+}
+
+function Modal({ title, closeBind, saveBind }: IProps) {
+  const [newLabel, setnewLabel] = React.useState<string>("");
+
   return (
-    <div
-      className="absolute top-0 z-50 flex justify-center items-center "
-      style={{ background: `rgba(0,0,0,0.2)` }}
-    >
-      <div className="modal__card">
-        <div className="text-xl">My modal</div>
+    <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+
+        <span
+          className="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+        >
+          &#8203;
+        </span>
+
+        <div
+          className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="modal-headline"
+        >
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+                <InfoIcon color={"blue"} />
+              </div>
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3
+                  className="text-lg leading-6 font-medium text-gray-900"
+                  id="modal-headline"
+                >
+                  {title}
+                </h3>
+                <div className="my-2">
+                  <p className="text-sm text-gray-500">
+                    Are you sure you want to add a new label?. After this
+                    process you can add snippet files to the label corresonding.
+                  </p>
+                </div>
+
+                <div className="">
+                  <input
+                    value={newLabel}
+                    onChange={(e) => setnewLabel(e.target.value)}
+                    type="text"
+                    className="w-full px-3 focus:ring-2 rounded-lg focus:ring-blue-400 bg-gray-50 py-2 outline-none"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <button
+              type="button"
+              onClick={() => {
+                saveBind(newLabel);
+                closeBind(false);
+              }}
+              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
+            >
+              Save
+            </button>
+            <button
+              type="button"
+              onClick={() => closeBind(false)}
+              className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- frontend/src/containers/LabelBar/index.tsx
+++ frontend/src/containers/LabelBar/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { AddIcon } from "../../assets/icons";
+import { Modal } from "../../components";
 
-const states = [
+let states = [
   { id: 1, label: "TypeScript" },
   { id: 2, label: "JavaScript" },
   { id: 3, label: "Go Lang" },
@@ -9,44 +10,59 @@ const states = [
 
 const LabelBar: React.FC = () => {
   const [searchText, setSearchText] = React.useState<string>("");
+  const [openModal, setOpenModal] = React.useState<boolean>(false);
+
+  function addNewLabelFunc(payload: string) {
+    states = [...states, { id: Math.random() * 10, label: payload }];
+  }
 
   return (
-    <aside
-      className="uppercase p-3  inline-flex min-h-screen tracking-wide w-52"
-      style={{ backgroundColor: `#07223a`, color: `#fff` }}
-    >
-      <div className="w-full">
-        <div className="flex justify-between border-b border-gray-600">
-          <div>
-            <span className="text-sm ">Labels</span>
-          </div>
-
-          <div>
-            <AddIcon color={"white"} />
-          </div>
-        </div>
-
-        {states
-          .filter((label) => label.label.toLowerCase().includes(searchText))
-          .map((state) => (
-            <div className=" my-3 text-sm text-gray-500 normal-case">
-              <div className="h-6 cursor-pointer my-2">
-                <div className="hover:text-gray-400">{state.label} </div>
-              </div>
+    <React.Fragment>
+      <aside
+        className="uppercase p-3  inline-flex min-h-screen tracking-wide w-52"
+        style={{ backgroundColor: `#07223a`, color: `#fff` }}
+      >
+        <div className="w-full">
+          <div className="flex justify-between border-b border-gray-600">
+            <div>
+              <span className="text-sm ">Labels</span>
             </div>
-          ))}
 
-        <div className="bottom-0 absolute -ml-1 mb-2">
-          <input
-            type="text"
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value.toLowerCase())}
-            placeholder="search for labels..."
-            className="px-4 py-1 w-full text-sm rounded bg-gray-700 opacity-70 overflow-hidden"
-          />
+            <div onClick={() => setOpenModal(true)}>
+              <AddIcon color={"white"} />
+            </div>
+          </div>
+
+          {states
+            .filter((label) => label.label.toLowerCase().includes(searchText))
+            .map((state) => (
+              <div className=" my-3 text-sm text-gray-500 normal-case">
+                <div className="h-6 cursor-pointer my-2">
+                  <div className="hover:text-gray-400">{state.label} </div>
+                </div>
+              </div>
+            ))}
+
+          <div className="bottom-0 absolute -ml-1 mb-2">
+            <input
+              type="text"
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value.toLowerCase())}
+              placeholder="search for labels..."
+              className="px-4 py-1 w-full text-sm outline-none rounded bg-gray-700 opacity-70 overflow-hidden"
+            />
+          </div>
         </div>
-      </div>
-    </aside>
+      </aside>
+
+      {openModal && (
+        <Modal
+          title={"Add a label"}
+          closeBind={setOpenModal}
+          saveBind={addNewLabelFunc}
+        />
+      )}
+    </React.Fragment>
   );
 };
 
